ec3bd9275791fb48e4517d0b91d7defaeeb2149e
XIAO <jinxiao.shawn@gmail.com>
2021-06-23 11:32:19 +0800

    Finishe the currency exchange service with h2 database.

---
+++ currency-exchange-service/src/main/java/com/xiao/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,37 @@
+package com.xiao.microservices.currencyexchangeservice;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class CurrencyExchange {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "currency_from")
+    private String from;
+
+    @Column(name = "currency_to")
+    private String to;
+
+    private BigDecimal conversionMultiple;
+
+    private String environment;
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+}
---
+++ currency-exchange-service/src/main/java/com/xiao/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,33 @@
+package com.xiao.microservices.currencyexchangeservice;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+    private final Environment environment;
+    private final CurrencyExchangeRepository repository;
+
+    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository repository) {
+        this.environment = environment;
+        this.repository = repository;
+    }
+
+    @GetMapping({"/currency-exchange/from/{from}/to/{to}"})
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
+
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+
+        return currencyExchange;
+    }
+}
